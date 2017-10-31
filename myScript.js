ad5317f14b15cbc0309225bed17a51233c0fddea
Nick Schneider <nac2@live.missouristate.edu>
2017-10-31 10:54:59 -0500

    js

---
+++ myScript.js
@@ -0,0 +1,17 @@
+function dataValidation() 
+    {
+        var x = document.forms["contactForm"]["firstname"].value;
+        var y = document.forms["contactForm"]["lastname"].value;
+
+        if (!isNaN(x) || x == "")
+        {
+            alert("Please enter your first name using characters only.")
+            return false;
+        }
+
+        if (!isNaN(y) || y == "")
+        {
+            alert("Please enter your last name using characters only.")
+            return false;
+        }
+    }
