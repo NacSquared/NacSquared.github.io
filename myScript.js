e1d27daa804262d4d80a61df4750e8a770120479
NacSquared <nac2@live.missouristate.edu>
2017-11-06 14:15:54 -0600

    Added interactive Menu

--- myScript.js
+++ myScript.js
@@ -17,4 +17,23 @@ function dataValidation()
         }
     }
 
+function dropDown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if(!event.target.matches(".dropbtn")){
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++){
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
+
 document.getElementById("date").innerHTML = new Date();
